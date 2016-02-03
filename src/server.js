d0de7ff6df8954c78906b9f77cf9a1be06564123
Ross O'Reilly <oreillyross@gmail.com>
2016-02-03 23:58:42 +0100

    more node

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 "use strict"
 
 var http = require('http')
+var fs = require('fs')
+
 
 http.createServer(function(req,res) {
 
-  res.writeHead(200, {'content-type': 'text/plain'})
-  res.end("Hello World\n")
+if (req.url === '/') {
+    res.writeHead(200, {'content-type': 'text/html'})
+    fs.createReadStream(__dirname + '/index.html', 'utf-8').pipe(res)
+}
+
+if (req.url === '/api') {
+    res.writeHead(200, {'content-type': 'application/json'})
+
+      var obj = {
+        first: 'John',
+        last: 'Doe'
+      }
+
+      res.end(JSON.stringify(obj))
+}
+
 }).listen(8080, '127.0.0.1')
