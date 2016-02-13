7fbffa931b8b4457078cfda048fb29dacf554274
Ross O'Reilly <oreillyross@gmail.com>
2016-02-13 15:03:38 +0100

    test data

---
+++ test/events.js
@@ -0,0 +1,26 @@
+[
+
+  {
+    name: "Food Truck Festival",
+    dates: {
+
+    }
+    image: "streamed image"
+
+
+  },
+  {
+    name: "Chinese New Year",
+    dates: {
+      start: ""
+    },
+    location: "adress from google",
+    image: "streamed image"
+
+
+  }
+
+
+
+
+]
