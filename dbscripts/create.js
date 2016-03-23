f18cee12c9ee42312ae7eb780d7b6f7b2461b241
Ross O'Reilly <oreillyross@gmail.com>
2016-03-23 07:57:54 +0000

    mongo scripts

---
+++ dbscripts/create.js
@@ -0,0 +1,6 @@
+use events;
+
+events.insert({"title": "Food truck festival", "date": "201603010900", "location": "Westebroek Park"});
+events.insert({"title": "Jazz Festival", "date": "201603020900", "location": "Rotterdam harbour"});
+events.insert({"title": "Fleamarket", "date": "201603010900", "location": "Zeeheldenquartier"});
+
